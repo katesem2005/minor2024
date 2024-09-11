b3594c5af70d8c49af64917600f5f9af95fd49ef
Екатерина <91794598+katesem2005@users.noreply.github.com>
2024-09-11 14:21:59 +0300

    Семинар

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML> <!--сообщаем браузеру, что используем HTML5 -->
+<html>
+<head><!--шапка документа со служебной информацией о страничке-->
+	<title>Моя первая страничка</title>
+	<meta charset='utf-8'> <!-- международная кодировка-->
+	<link href='css/style.css' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css2?family=Pattaya&display=swap' rel='stylesheet'>
+</head>
+<body>
+	<main> 
+		<header>
+		</header>
+		<nav>
+			<ul>
+				<li><a href=''>Главная страница</a></li>
+				<li><a href=''>Статьи</a></li>
+				<li><a href=''>Проекты</a></li>
+				<li><a href=''>Галерея</a></li>
+				<li><a href=''>О сайте</a></li>
+			</ul>
+		</nav>
+		<div id='sidebar'>
+			<ul>
+				<li><a href=''>Элемент1</li>
+				<li><a href=''>Элемент2</a></li>
+				<li><a href=''>Элемент3</a></li>
+				<li><a href=''>Элемент4</a></li>
+				<li><a href=''>Элемент5</a></li>
+			</ul>
+		</div>
+		<div id='content'>
+			<p>
+				Здесь размещена одна из статей
+			</p>
+		</div>
+
+		<footer>
+			Подвал
+		</footer>
+	</main>
+</body>
+</html>
